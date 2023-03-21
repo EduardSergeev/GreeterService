7aad56b27a4f55f373e39713941805d954e71edd
Eduard Sergeev <eduard.sergeev@gmail.com>
2023-03-21 17:13:43 +1100

    More streaming examples
    - Two-argument method: no client streaming (for now)
    - Single-argument of `IEnumerable<(Person, string[])>`: client streaming
    
    Also refactor `Version` handling in server

--- Greeter.Common/IGreeterExtendedService.cs
+++ Greeter.Common/IGreeterExtendedService.cs
@@ -5,5 +5,6 @@ public interface IGreeterExtendedService
     Greeting SayGreeting(Person person);
     IEnumerable<Greeting> SayGreetings(IEnumerable<Person> people);
     IEnumerable<string> StreamGreeting(Person person);
-    IEnumerable<string> StreamGreetings(IEnumerable<Person> people);
+    IEnumerable<string> StreamGreetings(IEnumerable<Person> people, string[] delimiter);
+    IEnumerable<string> StreamGreetingsEx(IEnumerable<(Person Person, string[] Footer)> people);
 }
