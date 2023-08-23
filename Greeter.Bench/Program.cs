5e447092e0736f22fa0ff95de839e2b9a7494ae6
Eduard Sergeev <eduard.sergeev@gmail.com>
2023-08-23 12:55:35 +1000

    Upload benchmark results to `docs` branch
    From the following jobs:
    - `bench-linux`
    - `bench-windows`
    Also add a couple of generated badges to `docs`

--- Greeter.Bench/Program.cs
+++ Greeter.Bench/Program.cs
@@ -1,8 +1,9 @@
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 using Greeter.Bench;
+using static System.Environment;
 
 BenchmarkRunner.Run<Benchmark>(ManualConfig
-    .Create(DefaultConfig.Instance.WithArtifactsPath(Path.Combine("..", ".bench")))
+    .Create(DefaultConfig.Instance.WithArtifactsPath(Path.Combine("..", ".bench", GetEnvironmentVariable("BENCH_RESULT_SUBPATH") ?? "")))
     .WithOptions(ConfigOptions.DisableOptimizationsValidator)
 );
